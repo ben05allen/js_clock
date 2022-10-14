40cdd76babcedc5239c0019b6007bd790c15c2a6
Ben <ben05allen@hotmail.com>
2022-10-14 09:16:36 +0900

    first commit

---
+++ js/clock.js
@@ -0,0 +1,30 @@
+function showTime() {
+    let date = new Date();
+    let h = date.getHours();
+    let m = date.getMinutes();
+    let s = date.getSeconds();
+    let ampm = "AM";
+
+    if (h >= 12) {
+        h -= 12;
+        ampm = "PM";
+    }
+
+    if (h == 0) {
+        h = 12;
+    }
+
+    h = (h < 10) ? '0' + h : h;
+    m = (m < 10) ? '0' + m : m;
+    s = (s < 10) ? '0' + s : s;
+
+    let time = h + ':' + m + ':' + s + ' ' + ampm;
+
+    // document.getElementById("MyClockDisplay").innerText = time;
+    document.getElementById("MyClockDisplay").textContent = time;
+    
+    setTimeout(showTime, 1000);
+
+}
+
+showTime()
